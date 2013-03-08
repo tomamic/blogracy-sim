095601903fcee40dcb18f7f144f40623f6df91ad
mic <mic@mic-Studio-XPS-1340.(none)>
2013-03-08 15:35:08 +0100

    log table

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from __future__ import print_function
+import sys
 import MySQLdb
 
 db = MySQLdb.connect('127.0.0.1', 'root', '', 'blogracy-sim')
 cursor = db.cursor()
 
+cycles = 50
+if len(sys.argv) >= 2:
+    cycles = int(sys.argv[1])
+
 sql = open('cycle.sql').read().split('\n')
-for i in range(50):
+for i in range(cycles):
     for x in sql:
         if len(x) > 0 and x[0] != '#':
             print(x)
